ec4ebd780636e1f8339fab328df161742e4c0b44
fozery <fozery@yandex.ru>
2024-06-27 20:18:41 +0300

    feat: add AppButton

---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    disabled: { type: Boolean, default: false },
+})
+
+defineEmits(['click'])
+</script>
+
+<template>
+    <button
+        @click="$emit('click')"
+        class="rounded-3xl shadow-md px-3 py-1 transition duration-300"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped></style>
