52971d8c00779bb6c89e10ace882e8361d34526e
fozery <fozery@yandex.ru>
2024-06-29 17:54:56 +0300

    feat: add layouts

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import { useUserStore } from '@/stores/userStore.js'
-import router from '@/router/router.js'
-import { onBeforeRouteUpdate } from 'vue-router'
-import AppLoader from '@/components/ui/AppLoader.vue'
-
-const userStore = useUserStore()
-
-const isLoading = ref(true)
-
-onMounted(() => {
-    userStore
-        .check()
-        .then(async (data) => {
-            userStore.user = data
-            userStore.isAuth = true
-        })
-        .finally(() => {
-            isLoading.value = false
-        })
-})
+import AppLayout from '@/layouts/AppLayout.vue'
 </script>
 
 <template>
-    <div
-        v-if="isLoading"
-        class="w-full h-full flex justify-center items-center pb-32"
-    >
-        <AppLoader v-if="!isLoading" />
-    </div>
-    <router-view v-else></router-view>
+    <AppLayout>
+        <router-view></router-view>
+    </AppLayout>
 </template>
 
 <style scoped></style>
